50c3e5d874420ed8f61090584ad82e266c13e7a3
aiden3918 <liaiden3918@gmail.com>
2026-02-13 12:37:42 -0800

    added enemy tile anims

--- Assets/Scripts/EnemyAttackTileObject.cs
+++ Assets/Scripts/EnemyAttackTileObject.cs
@@ -28,6 +28,7 @@ public class EnemyAttackTileObject : MonoBehaviour
     private Vector2[] parriedBezierPoints;
 
     [SerializeField] private TextMeshProUGUI tmpElement;
+    [SerializeField] private TextMeshProUGUI label;
 
     private void Awake()
     {
@@ -38,22 +39,56 @@ public class EnemyAttackTileObject : MonoBehaviour
     public void Initialize(Tile tile, Vector2 initPos, Vector2 offset)
     {
         tileData = tile;
+        if (tileData.baseTileData.faceSprite == null )
+        {
+            tmpElement.text = tile.rank.ToString() + " of " + tile.suit.ToString();
+            tileFaceImage.enabled = false;
+            label.enabled = false;
+        }
+        else
+        {
+            tmpElement.enabled = false;
+            tileFaceImage.sprite = tileData.baseTileData.faceSprite;
+            label.text = tile.baseTileData.suit switch
+            {
+                TileSuit.None => "X",
+                TileSuit.Bamboo => tile.rank.ToString(),
+                TileSuit.Dot => tile.rank.ToString(),
+                TileSuit.Character => tile.rank.ToString(),
+                TileSuit.Wind => tile.rank switch
+                {
+                    1 => "N",
+                    2 => "E",
+                    3 => "S",
+                    4 => "W",
+                    _ => "X"
+                },
+                TileSuit.Dragon => tile.rank switch
+                {
+                    1 => "G",
+                    2 => "R",
+                    3 => "W",
+                    _ => "X"
+                },
+                _ => "X"
+            };
+        }
+
         idleOffset = offset;
         initialPosition = initPos;
         rt.position = initialPosition;
         initialScale = new(rt.rect.width, rt.rect.height);
         tmpElement.text = tile.rank.ToString() + " of " + tile.suit.ToString();
 
-        Vector2 drawBezierOffset = new(initPos.x + 100.0f, initPos.y);
+        Vector2 drawBezierOffset = new(initPos.x + 100.0f, initPos.y - 20.0f);
         drawBezierPoints = new Vector2[] {initPos, drawBezierOffset, initPos + offset};
 
-        Vector2 attackBezierOffset = new(initPos.x + 200.0f, initPos.y);
-        Vector2 attackFinalPoint = new(initPos.x, initPos.y - 100.0f);
+        Vector2 attackBezierOffset = new(initPos.x + 350.0f, initPos.y + 100.0f);
+        Vector2 attackFinalPoint = new(initPos.x + 50.0f, initPos.y - 500.0f);
         attackBezierPoints = new Vector2[] {drawBezierPoints[2], attackBezierOffset, attackFinalPoint};
 
-        Vector2 parryBezierOffset = new(initPos.x, initPos.y + 250.0f);
-        Vector2 parryFinalPoint = new(initPos.x, initPos.y + 200.0f);
-        parriedBezierPoints = new Vector2[] {drawBezierPoints[2], attackBezierOffset, attackFinalPoint};
+        Vector2 parryBezierOffset = new(drawBezierPoints[2].x + 150.0f, drawBezierPoints[2].y + 250.0f);
+        parriedBezierPoints = new Vector2[] {drawBezierPoints[2], parryBezierOffset, initPos};
     }
 
     public IEnumerator PlayDrawAnimation()
@@ -73,7 +108,7 @@ public class EnemyAttackTileObject : MonoBehaviour
             rt.anchoredPosition += tileVel * spf;
             // rt.anchoredPosition = Utils.PointOnQuadraticBezierCurve2D(t, drawBezierPoints[0], drawBezierPoints[1], drawBezierPoints[2], attackAnimationDuration);
 
-            float tileRotationAngle = -90.0f * (float)Math.Exp(-2.2f * t / drawAnimationDuration);
+            float tileRotationAngle = -90.0f * (float)Math.Exp(-3.0f * t / drawAnimationDuration);
             if (Math.Abs(tileRotationAngle) < 5.0f) tileRotationAngle = 0.0f;
             rt.rotation = Quaternion.Euler(0.0f, 0.0f, tileRotationAngle);
 
@@ -94,6 +129,8 @@ public class EnemyAttackTileObject : MonoBehaviour
 
             Vector2 tileVel = Utils.SlopeOnQuadraticBezierCurve2D(t, attackBezierPoints[0], attackBezierPoints[1], attackBezierPoints[2], attackAnimationDuration);
             rt.anchoredPosition += tileVel * spf;
+            rt.sizeDelta = initialScale * (float)Math.Exp(0.35f * t);
+            rt.rotation = Quaternion.Euler(0.0f, 0.0f, -180.0f * (1.0f - (float)Math.Exp(-3.0f * t / drawAnimationDuration)));
             // rt.anchoredPosition = Utils.PointOnQuadraticBezierCurve2D(t, drawBezierPoints[0], drawBezierPoints[1], drawBezierPoints[2], attackAnimationDuration);
             
             yield return new WaitForSeconds(spf);
@@ -114,9 +151,7 @@ public class EnemyAttackTileObject : MonoBehaviour
             rt.anchoredPosition += tileVel * spf;
             // rt.anchoredPosition = Utils.PointOnQuadraticBezierCurve2D(t, drawBezierPoints[0], drawBezierPoints[1], drawBezierPoints[2], attackAnimationDuration);
 
-            float tileRotationAngle = -180.0f *(1.0f - (float)Math.Exp(-2.0f * t / parriedAnimationDuration)); 
-            if (Math.Abs(tileRotationAngle) > 175.0f) tileRotationAngle = 180.0f;
-            rt.rotation = Quaternion.Euler(0.0f, 0.0f, tileRotationAngle);
+            rt.rotation = Quaternion.Euler(0.0f, 0.0f, 225.0f * (1.0f - (float)Math.Exp(-1.0f * t / drawAnimationDuration)));
             
             yield return new WaitForSeconds(spf);
         }
