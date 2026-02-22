a52ca303e06b3e70b4cb7dab2e9df0f7943abbad
RyanLle <ryanlee5690@gmail.com>
2026-02-22 14:42:43 -0800

    added type to flower tile data
    forgot to add this with the other

--- Assets/Scripts/FlowerTiles/FlowerTileData.cs
+++ Assets/Scripts/FlowerTiles/FlowerTileData.cs
@@ -10,6 +10,7 @@ public enum FlowerTileType
     EightFourBonus, //Multiply damgage positively for each 8 and negatively for each 4
     RandomWindBonus, //Multiply damage positively for each wind tile that matches the current round's wind
     QiBonus, //Give bonus damage according to amount of qi
+    TileBonus, //Give bonus damage according to amount of tiles selected
 }
 
 [CreateAssetMenu(fileName = "New Flower Tile", menuName = "Flower Tile")]
