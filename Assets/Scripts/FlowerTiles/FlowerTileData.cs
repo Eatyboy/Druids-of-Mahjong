47ec5527dcedfccea38fe5c7a4198efd261ca898
RyanLle <ryanlee5690@gmail.com>
2026-02-22 14:38:07 -0800

    created damage multiplier based if enemy is at full hp
    also has a parameter in the prefab

--- Assets/Scripts/FlowerTiles/FlowerTileData.cs
+++ Assets/Scripts/FlowerTiles/FlowerTileData.cs
@@ -10,6 +10,7 @@ public enum FlowerTileType
     EightFourBonus, //Multiply damgage positively for each 8 and negatively for each 4
     RandomWindBonus, //Multiply damage positively for each wind tile that matches the current round's wind
     QiBonus, //Give bonus damage according to amount of qi
+    EnemyFullHealthBonus, //Multiply damage positively if enemy is at full hp
 }
 
 [CreateAssetMenu(fileName = "New Flower Tile", menuName = "Flower Tile")]
