c988d3917e50f1d8e92d6ce226770ac1374906b1
TobbyTukaywan <abdurrahman.reed.syed@gmail.com>
2026-02-25 09:14:03 -0800

    Implement ScrollHand

---
+++ Assets/Scripts/CharmScrollType.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// Identifies which charm scroll action to run. Used with CharmScrollDefinition and CharmScrollRunner.
+/// </summary>
+public enum CharmScrollType
+{
+    RemoveTiles,
+    AddCopies,
+    SwitchToBamboo,
+    SwitchToDot,
+    SwitchToCharacter,
+    IncreaseMaxHealth,
+    DoubleQi,
+}
